2135757580fdefed6267aacd8b9ea1b6b21b1195
Joeperdefloep <joepgevaert@gmail.com>
2021-01-01 20:05:30 +0100

    added script to load .mdb database files -> using mdbtools sudo apt install mdbtools

---
+++ ExportMDB.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 24 14:46:54 2020
+based on https://stackoverflow.com/a/42226003
+requires mdbtools to be installed. `sudo apt install mdbtools`
+@author: joempie
+"""
+import sys, subprocess, os
+from io import StringIO
+import pandas
+VERBOSE = True
+
+def mdb_to_pandas(database_path):
+    subprocess.call(["mdb-schema", database_path, "mysql"])
+    # Get the list of table names with "mdb-tables"
+    table_names = subprocess.Popen(["mdb-tables", "-1", database_path],
+                                   stdout=subprocess.PIPE).communicate()[0]
+    tables = table_names.splitlines()
+    sys.stdout.flush()
+    # Dump each table as a stringio using "mdb-export",
+    out_tables = {}
+    for rtable in tables:
+        table = rtable.decode()
+        if VERBOSE: print('running table:',table)
+        if table != '':
+            if VERBOSE: print("Dumping " + table)
+            contents = subprocess.Popen(["mdb-export", database_path, table],
+                                        stdout=subprocess.PIPE).communicate()[0]
+            temp_io = StringIO(contents.decode())
+            print(table, temp_io)
+            out_tables[table] = pd.read_csv(temp_io)
+    return out_tables
+
+inputDB = mdb_to_pandas('/home/joempie/FarmDESIGN/Input/FarmDB.mdb')
